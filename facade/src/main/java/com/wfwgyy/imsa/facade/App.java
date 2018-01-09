be7b4892d938bd82a381fc4e996a89f647d3f6b8
Yan Tao <yt7589@qq.com>
2018-01-09 16:32:19 +0800

    add the facade project: It will receive all the external RESTful request and response to it

---
+++ facade/src/main/java/com/wfwgyy/imsa/facade/App.java
@@ -0,0 +1,13 @@
+package com.wfwgyy.imsa.facade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "微服务工业云平台..." );
+    }
+}
