2e232b3d6d789f920517a60f5422f62273c9cade
Yan Tao <yt7589@qq.com>
2018-02-01 20:08:06 +0800

    fix a temp BUG

--- universe/common/src/main/java/com/wfwgyy/imsa/common/App.java
+++ universe/common/src/main/java/com/wfwgyy/imsa/common/App.java
@@ -11,7 +11,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "AioTcpClient" );
         AioTcpClient.start(AppConsts.FACADE_HOST, AppConsts.FACADE_PORT);
         try {
 			Thread.sleep(2*1000);
