1a5b16323d8dce306953c8ff2e03b7d38b24355d
Yan Tao <yt7589@qq.com>
2018-02-02 10:28:16 +0800

    move server configuration to AppConsts

--- universe/common/src/main/java/com/wfwgyy/imsa/common/AppConsts.java
+++ universe/common/src/main/java/com/wfwgyy/imsa/common/AppConsts.java
@@ -1,6 +1,8 @@
 package com.wfwgyy.imsa.common;
 
 public class AppConsts {
+	// 服务器配置
+	public final static String SERVER_IP = "192.168.11.15";
 	// 系统消息所需属性
 	public final static String MSG_ID = "msg_id";
 	public final static String MSG_TYPE = "msg_type";
@@ -18,12 +20,12 @@ public class AppConsts {
 	public final static int MT_MSG_V4 = 4;
 	public final static int MT_MSG_V5 = 5;
 	// Redis服务器
-	public final static String REDIS_HOST = "192.168.223.38";
+	public final static String REDIS_HOST = SERVER_IP;
 	public final static short REDIS_PORT = 6379;
 	// 消息服务器相关配置
-	public final static String PLATO_HOST = "192.168.223.38";
+	public final static String PLATO_HOST = SERVER_IP;
 	public final static short PLATO_PORT = 8089;
 	// 门户Facade相关
-	public final static String FACADE_HOST = "192.168.223.38";
+	public final static String FACADE_HOST = SERVER_IP;
 	public final static short FACADE_PORT = 8088;
 }
